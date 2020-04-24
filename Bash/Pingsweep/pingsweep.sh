5484650629885ec54efe8d9439b4d1775a825260
agnusdei <20711174+RealisAgnusDei@users.noreply.github.com>
2020-04-24 14:35:11 +0200

    Added comments

--- Bash/Pingsweep/pingsweep.sh
+++ Bash/Pingsweep/pingsweep.sh
@@ -10,6 +10,7 @@ that respond to the echo-request.
 COMMENT
 
 
+# Checks if an argument for interface has been given. If none has been given, exits script and notifies user.
 if [ "$1" == "" ]
 then
 	printf "You need to specify which interface to get your IP from!\nSyntax: ./script.sh <interface>\n"
@@ -21,6 +22,7 @@ fi
 
 interface=$1
 
+# Function for obtaining the IP-address associated with the interface and formatting the resulting string correctly. If no interface with the specified interface exists, exits script and notifies user. 
 get_ip()
 {
 	ip=$(ip addr show $interface | awk '/inet/{print ($2)}' | awk 'FNR==1' \
@@ -32,6 +34,7 @@ get_ip()
 }
 
 
+# Function for perfoming ping against IP-addresses recieved from the for-loop at end of script. If the ping is successful, prints IP-address to STDOUT.
 ping_ip()
 {
 	ping -c 2 $1 > /dev/null
@@ -44,6 +47,7 @@ ping_ip()
 
 get_ip
 
+#Main loop
 for i in $ip.{1..254}
 do
 	ping_ip $i &
